1d80f580c6d3e414b94855d28a3bd625db0d86d6
Torimitsu <132206022+ultsaza@users.noreply.github.com>
2023-05-05 10:39:43 +0000

    [tessoku-book] tessoku_book_ch

--- tessoku-book/tessoku_book_ch/tessoku_book_ch.cpp
+++ tessoku-book/tessoku_book_ch/tessoku_book_ch.cpp
@@ -1,44 +1,39 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
- 
-// 入力で与えられる変数
-int N;
-int A[100009], B[100009], C[100009], D[100009];
- 
-// 座標 (i+0.5, j+0.5) に置かれている紙の数 T[i][j]
-int T[1509][1509];
- 
+
 int main() {
-	// 入力
-	cin >> N;
-	for (int i = 1; i <= N; i++) cin >> A[i] >> B[i] >> C[i] >> D[i];
- 
-	// 各紙について +1/-1 を加算
-	for (int i = 0; i <= 1500; i++) {
-		for (int j = 0; j <= 1500; j++) T[i][j] = 0;
-	}
-	for (int i = 1; i <= N; i++) {
-		T[A[i]][B[i]] += 1;
-		T[A[i]][D[i]] -= 1;
-		T[C[i]][B[i]] -= 1;
-		T[C[i]][D[i]] += 1;
-	}
- 
-	// 二次元累積和をとる
-	for (int i = 0; i <= 1500; i++) {
-		for (int j = 1; j <= 1500; j++) T[i][j] = T[i][j - 1] + T[i][j];
-	}
-	for (int i = 1; i <= 1500; i++) {
-		for (int j = 0; j <= 1500; j++) T[i][j] = T[i - 1][j] + T[i][j];
-	}
- 
-	// 面積を数える
-	int Answer = 0;
-	for (int i = 0; i <= 1500; i++) {
-		for (int j = 0; j <= 1500; j++) {
-			if (T[i][j] >= 1) Answer += 1;
-		}
-	}
-	cout << Answer << endl;
-	return 0;
+  int n;
+  int a[100009],b[100009],c[100009],d[100009];
+  int x[1509][1509];
+  cin >> n;
+  
+  for(int i=0;i<=1500;i++) {
+    for(int j=0;j<=1500;j++) {
+      x[i][j]=0;
+    }
+  }
+  for(int i=1;i<=n;i++) {
+    cin >> a[i] >> b[i] >> c[i] >> d[i];
+    x[a[i]][b[i]] ++;
+    x[a[i]][d[i]] --;
+    x[c[i]][b[i]] --;
+    x[c[i]][d[i]] ++;
+  }
+  for(int i=0;i<=1500;i++) {
+    for(int j=1;j<=1500;j++) {
+      x[i][j]=x[i][j-1]+x[i][j];
+    }
+  }
+  for(int i=1;i<=1500;i++) {
+    for(int j=0;j<=1500;j++) {
+      x[i][j]=x[i][j]+x[i-1][j];
+    }
+  }
+  int A=0;
+  for(int i=0;i<=1500;i++) {
+    for(int j=0;j<=1500;j++) {
+      if(x[i][j] >= 1) A++;
+    }
+  }
+  cout << A << endl;
 }
