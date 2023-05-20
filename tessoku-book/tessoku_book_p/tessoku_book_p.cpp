e6f451ae3ccc32b4af79eee5bcee81385da3eb40
Torimitsu <132206022+ultsaza@users.noreply.github.com>
2023-05-20 10:13:26 +0000

    [tessoku-book] tessoku_book_p

--- tessoku-book/tessoku_book_p/tessoku_book_p.cpp
+++ tessoku-book/tessoku_book_p/tessoku_book_p.cpp
@@ -1,19 +1,16 @@
 #include<iostream>
 #include<algorithm>
 using namespace std;
-
 int main() {
-  int n,a[100009],b[100009];
-  int dp[100009];
+  int n,a[100009],b[100009],dp[100009];
   cin >> n;
   for(int i=2;i<=n;i++) {
-    cin >> a[i];
+    cin >> a[i]; dp[i]=-1000000;
   }
   for(int i=3;i<=n;i++) {
     cin >> b[i];
   }
-  dp[1]=0;
-  dp[2]=a[2];
+  dp[1]=0; dp[2]=a[2];
   for(int i=3;i<=n;i++) {
     dp[i]=min(dp[i-1]+a[i],dp[i-2]+b[i]);
   }
