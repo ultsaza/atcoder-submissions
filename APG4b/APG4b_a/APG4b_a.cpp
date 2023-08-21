4400e3d85de52226b0d630fb8f5602089c4b37fa
Torimitsu <132206022+ultsaza@users.noreply.github.com>
2023-08-21 12:28:46 +0000

    [APG4b] APG4b_a

--- APG4b/APG4b_a/APG4b_a.cpp
+++ APG4b/APG4b_a/APG4b_a.cpp
@@ -1,6 +1,6 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-	int main() {
-      cout << "Hello, world!" << endl;
-    }
+int main() {
+  cout << "Hello, world!" << endl;
+}
