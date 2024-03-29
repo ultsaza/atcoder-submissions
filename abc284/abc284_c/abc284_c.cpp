b62679063a0a7c4f7d3e671aa9a3d8d2edee4155
Torimitsu <132206022+ultsaza@users.noreply.github.com>
2024-03-29 16:24:00 +0000

    [abc284] abc284_c

--- abc284/abc284_c/abc284_c.cpp
+++ abc284/abc284_c/abc284_c.cpp
@@ -1,44 +1,46 @@
-#include<bits/stdc++.h>
-#include<atcoder/all>
+#include <bits/stdc++.h>
+#include <atcoder/all>
 using namespace std;
 using namespace atcoder;
-using ll=long long;
-#define rep(i, n) for (int i = 1; i <= (int)(n); i++)
-#define rep2(i, s, n) for (int i = (s); i <= (int)(n); i++)
-const ll M=1000000007;
+using ll = long long;
+#define overload4(_1, _2, _3, _4, name, ...) name
+#define rep1(n) for (ll i = 0; i < (n); ++i)
+#define rep2(i, n) for (ll i = 0; i < (n); ++i)
+#define rep3(i, a, b) for (ll i = (a); i < (b); ++i)
+#define rep4(i, a, b, c) for (ll i = (a); i < (b); i += (c))
+#define rep(...) overload4(__VA_ARGS__, rep4, rep3, rep2, rep1)(__VA_ARGS__)
+#define all(i) begin(i), end(i)
+const int INF = 0x3fffffff;
+const ll LINF = 0x1ffffffffffffff;
 
-int n,m;
+
+int N,M;
+vector<vector<int>> G;
+vector<bool> visited;
+int ans = 0;
+
+void dfs(int v) {
+    visited[v] = 1;
+    for(auto nv:G[v]) {
+        if(visited[nv]) continue;
+        dfs(nv);
+    }
+}
+
 int main() {
-    cin >> n >> m;
-    vector<vector<int>> g(n+1);
-    rep(i,m) {
-        int u,v;
-        cin >> u >> v;
-        g[u].emplace_back(v);
-        g[v].emplace_back(u);
+    cin >> N >> M;
+    G.resize(N);
+    visited.resize(N,0);
+    rep(i,M) {
+        int v,u;
+        cin >> v >> u;
+        G[v-1].push_back(u-1);
+        G[u-1].push_back(v-1);
     }
-    bool seen[n+9];
-    rep(i,n) seen[i]=false;
-    queue<int> todo;
-    int ans=0;
-    // sは調べる起点,s起点のグラフが連結成分の一つと確定したら，　まだ連結成分の判定をしてないグラフの頂点の一つをsとする。
-    //seen[u]==true　は連結成分の判定が終わったことを表す。
-    rep(i,n) {
-        if(seen[i]==false) {
-            todo.push(i);
-            seen[i]=true;
-            while(!todo.empty()) {
-                int v = todo.front();
-                todo.pop();
-                for(int u: g[v]) {
-                    if(seen[u] == false) {
-                        seen[u] = true;
-                        todo.push(u);
-                    }
-                }
-             }
-            ans++;
-        }
+    rep(i,N) {
+        if(visited[i]) continue;
+        dfs(i);
+        ans++;
     }
-    cout << ans;
+    cout << ans << endl;
 }
