4a4fe6b5120977cf85084ae1a4a5eb93f4cebd63
Torimitsu <132206022+ultsaza@users.noreply.github.com>
2024-04-08 09:43:57 +0000

    [abc213] abc213_d

--- abc213/abc213_d/abc213_d.cpp
+++ abc213/abc213_d/abc213_d.cpp
@@ -1,40 +1,45 @@
-#include<bits/stdc++.h>
-#include<atcoder/all>
+#include <bits/stdc++.h>
+#include <atcoder/all>
 using namespace std;
 using namespace atcoder;
-using ll=long long;
-#define overload4(_1, _2, _3, _4, name, ...) name
-#define rep1(n) for(ll i = 0; i < (n); ++i)
-#define rep2(i, n) for(ll i = 0; i < (n); ++i)
-#define rep3(i, a, b) for(ll i = (a); i < (b); ++i)
-#define rep4(i, a, b, c) for(ll i = (a); i < (b); i += (c))
-#define rep(...) overload4(__VA_ARGS__, rep4, rep3, rep2, rep1)(__VA_ARGS__)
-#define all(i) begin(i), end(i)
-const ll LINF = 0x1fffffffffffffff;
+using ll = long long;
+#define rep(i, l, r) for (ll i = (l); i < (r); ++i)
+#define all(x) (x).begin(), (x).end()
+const ll INF = 0x1ffffffffffffff;
 
+int N;
 vector<vector<int>> G;
-vector<int> ans;
+vector<int>ans;
 
-void dfs(int crr,int pre) {
-    ans.push_back(crr);//行き止まりに行った時戻る動作に対応させることができる
-    //行き止まり前の頂点についたら一個前の頂点の他の隣接頂点に移動する操作に入る
-    //その時にans.push_back(crr)で戻る操作を実現できる
-    for(auto v : G[crr]) {
-        if(v == pre) continue;
-        dfs(v,crr);
-        ans.push_back(crr);
-    }
+void dfs(int crr,int pre){
+  ans.push_back(crr);
+  for(int nxt:G[crr])if(nxt!=pre){
+    dfs(nxt,crr);
+    ans.push_back(crr);
+  }
 }
 
+
 int main() {
-    int N; cin >> N;
-    G.resize(N+1);//これしてあげないとG[i]に要素追加できない
-    rep(i,N-1) {
-        int a,b; cin >> a >> b;
+    cin >> N;
+    G.resize(N);
+    rep(i,0,N-1) {
+        int a,b;
+        cin >> a >> b;
+        a--; b--;
         G[a].push_back(b);
         G[b].push_back(a);
     }
-    rep(i,1,N+1) sort(all(G[i]));
-    dfs(1,-1);
-    rep(i,ans.size()) cout << ans[i] <<" ";
+    rep(i,0,N) {
+        sort(all(G[i]));
+    }
+    dfs(0, -1);
+  
+  
+  for(auto u: ans) {
+    cout << ++u << ' ';
+  }
+
 }
+
+
