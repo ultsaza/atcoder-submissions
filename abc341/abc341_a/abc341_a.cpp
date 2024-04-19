3a17d89f69e1849a3b72053fab3f73bf233f2406
Torimitsu <132206022+ultsaza@users.noreply.github.com>
2024-04-19 07:42:59 +0000

    [abc341] abc341_a

--- abc341/abc341_a/abc341_a.cpp
+++ abc341/abc341_a/abc341_a.cpp
@@ -1,24 +1,17 @@
-#include<bits/stdc++.h>
-#include<atcoder/all>
+#include <bits/stdc++.h>
+#include <atcoder/all>
 using namespace std;
 using namespace atcoder;
-using ll=long long;
-#define overload4(_1, _2, _3, _4, name, ...) name
-#define rep1(n) for(ll i = 0; i < (n); ++i)
-#define rep2(i, n) for(ll i = 0; i < (n); ++i)
-#define rep3(i, a, b) for(ll i = (a); i < (b); ++i)
-#define rep4(i, a, b, c) for(ll i = (a); i < (b); i += (c))
-#define rep(...) overload4(__VA_ARGS__, rep4, rep3, rep2, rep1)(__VA_ARGS__)
-#define all(i) begin(i), end(i)
-const int INF = 0x3fffffff;
-const ll LINF = 0x1ffffffffffffff;
-
+using ll = long long;
+#define rep(i, l, r) for (ll i = (l); i < (r); ++i)
+#define all(x) (x).begin(), (x).end()
+const ll INF = 0x1ffffffffffffff;
 
 int main() {
     int N;
-    cin >> N ;
-    rep(i,N) {
-        cout << 10;
+    cin >> N;
+    rep(i,0,N) {
+        cout << 1 << 0;
     }
-    cout << 1 << endl;
+    cout << 1;
 }
