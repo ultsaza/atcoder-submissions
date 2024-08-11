70fab46f2a1ba5d98775c0ecd9044d59e7cfd470
Torimitsu <132206022+ultsaza@users.noreply.github.com>
2024-08-11 16:10:11 +0000

    [typical90] typical90_a

--- typical90/typical90_a/typical90_a.cpp
+++ typical90/typical90_a/typical90_a.cpp
@@ -1,55 +1,40 @@
 #include <bits/stdc++.h>
-#include <atcoder/all>
 using namespace std;
-using namespace atcoder;
 using ll = long long;
-#define overload4(_1, _2, _3, _4, name, ...) name
-#define rep1(n) for (ll i = 0; i < (n); ++i)
-#define rep2(i, n) for (ll i = 0; i < (n); ++i)
-#define rep3(i, a, b) for (ll i = (a); i < (b); ++i)
-#define rep4(i, a, b, c) for (ll i = (a); i < (b); i += (c))
-#define rep(...) overload4(__VA_ARGS__, rep4, rep3, rep2, rep1)(__VA_ARGS__)
-#define all(i) begin(i), end(i)
-const int INF = 0x3fffffff;
-const ll LINF = 0x1ffffffffffffff;
-
-
+#define rep(i, l, r) for (ll i = (l); i < (r); ++i)
+#define all(x) (x).begin(), (x).end()
 
 int main() {
-    ll N,L,K;
-    cin >> N  >> L >> K;
-
-    vector<ll> A(N);
-    rep(N) cin >> A[i];
-    vector<ll> P(N+1);
-    P[0] = A[0];
-    P[N] = L - A[N-1];
-    rep(i,N-1) {
-        P[i+1] = A[i+1] - A[i];
+    ll n, l, k;
+    cin >> n >> l >> k;
+    ll a[n];
+    rep(i, 0, n) {
+        cin >> a[i];
     }
+    ll b[n + 1];
+    b[0] = a[0];
+    rep(i, 1, n) {
+        b[i] = a[i] - a[i - 1];
+    }
+    b[n] = l - a[n - 1];
 
-    ll l=0, r = 1e9;
-
-    while(abs(l-r)>1) {
-        ll mid = (l+r)/2;
-
-        //全ての切断がmid以上にできるかを判定
-
-        ll cur =0,cnt = 0;
-
-        rep(i,N+1) {
-            cur += P[i];
+    ll left = -1, right = (int)1e9 + 1, mid;
+    while(right - left > 1) {
+        mid = (left + right) / 2;
+        int count = 0;
+        ll cur = 0;
+        rep(i, 0, n + 1) {
+            cur += b[i];
             if(cur >= mid) {
-                cnt ++;
                 cur = 0;
+                count++;
             }
         }
-        if(cnt>=K+1) {
-            l = mid;
+        if(count > k) {
+            left = mid;
         } else {
-            r = mid;
+            right = mid;
         }
     }
-
-    cout << l << endl;
+    cout << left << endl;
 }
