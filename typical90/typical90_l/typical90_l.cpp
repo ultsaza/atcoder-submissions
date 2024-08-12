35834f9238bf6463d7c2f1137c1f7f61cc58ae8d
Torimitsu <132206022+ultsaza@users.noreply.github.com>
2024-08-12 06:25:03 +0000

    [typical90] typical90_l

--- typical90/typical90_l/typical90_l.cpp
+++ typical90/typical90_l/typical90_l.cpp
@@ -6,41 +6,13 @@ using ll = long long;
 #define rep(i, r) for (ll i = 0; i < (r); ++i)
 #define all(x) (x).begin(), (x).end()
 
-class uf {
-    public:
-            vector<int> par;
 
-            void init(int sz) {
-                par.resize(sz, -1);
-            }
-
-            int root(int pos) {
-                if(par[pos] == -1) {
-                    return pos;
-                }
-                par[pos] = root(par[pos]);
-                return par[pos];
-            }
-            void unite(int u, int v) {
-                u = root(u);
-                v = root(v);
-                if(u == v) return;
-                par[u] = v;
-            }
-            bool same(int u, int v) {
-                if(root(u) == root(v)) {
-                    return 1;
-                }
-                return 0;
-            }
-};
 
 int h, w, q;
-uf UF;
 
 bool used[2009][2009];
 
-void query1(int px, int py) {
+void query1(int px, int py, dsu &UF) {
     int dx[4] = {-1, 0, 1, 0};
     int dy[4] = {0, 1, 0, -1};
     rep(i,4) {
@@ -52,12 +24,12 @@ void query1(int px, int py) {
         }
         int hash1 = (px - 1) * w + (py - 1);
         int hash2 = (sx - 1) * w + (sy - 1);
-        UF.unite(hash1, hash2);
+        UF.merge(hash1, hash2);
     }
     used[px][py] = true;
 }
 
-bool query2(int px, int py, int qx, int qy) {
+bool query2(int px, int py, int qx, int qy, dsu &UF) {
     if(used[px][py] == false || used[qx][qy] == false) {
         return false;
     }
@@ -71,19 +43,19 @@ bool query2(int px, int py, int qx, int qy) {
 
 int main() {
     cin >> h >> w >> q;
-    UF.init(h * w);
+    dsu uf(h * w);
 
     rep(_, q) {
         int ty; cin >> ty;
         if(ty == 1) {
             int x, y;
             cin >> x >> y;
-            query1(x, y);
+            query1(x, y, uf);
         }
         if(ty == 2) {
             int xa, xb, ya, yb;
             cin >> xa >> xb >> ya >> yb;
-            bool ans = query2(xa, xb, ya, yb);
+            bool ans = query2(xa, xb, ya, yb, uf);
             if(ans == true) {
                 cout << "Yes" << endl;
             }
