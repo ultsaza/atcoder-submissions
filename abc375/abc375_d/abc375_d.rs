baf25ac01bf40149573ad977994e05a074b2758f
Torimitsu <132206022+ultsaza@users.noreply.github.com>
2025-05-24 19:22:14 +0000

    [abc375] abc375_d

--- abc375/abc375_d/abc375_d.rs
+++ abc375/abc375_d/abc375_d.rs
@@ -4,31 +4,18 @@ fn main() {
     input! {
         s: Bytes
     }
-    let mut cntl = vec![vec![0;s.len()+1];26];
-    let mut cntr = vec![vec![0;s.len()+1];26];
-    for i in 0..s.len() {
-        for j in 0..26 {
-            if s[i] == b'A' + j as u8 {
-                cntl[j][i+1] = cntl[j][i] + 1;
-            } else {
-                cntl[j][i+1] = cntl[j][i];
-            }
-        }
+    let mut cntl:Vec<usize> = vec![0;26];
+    let mut cntr:Vec<usize> = vec![0;26];
+    for c in s.iter() {
+        cntr[(c-b'A') as usize] += 1;
     }
-    for i in (0..s.len()).rev() {
-        for j in 0..26 {
-            if s[i] == b'A' + j as u8 {
-                cntr[j][i] = cntr[j][i+1] + 1;
-            } else {
-                cntr[j][i] = cntr[j][i+1];
-            }
+    let mut ans: usize = 0;
+    for c in s {
+        cntr[(c-b'A') as usize] -= 1;
+        for i in 0..26 {
+            ans += cntl[i] * cntr[i];
         }
-    }
-    let mut ans: u128 = 0;
-    for i in 1..s.len()-1 {
-        for c in 0..26 {
-            ans += cntl[c][i] * cntr[c][i+1];
-        }
+        cntl[(c-b'A') as usize] += 1;
     }
     println!("{}", ans);
 }
