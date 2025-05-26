1e31a02595ffe12d0988ba2db8628ec37f28614d
Torimitsu <132206022+ultsaza@users.noreply.github.com>
2025-05-26 22:20:54 +0000

    [abc323] abc323_e

--- abc323/abc323_e/abc323_e.rs
+++ abc323/abc323_e/abc323_e.rs
@@ -9,25 +9,24 @@ fn main() {
         t: [usize; n],
     }
     let mut ans = mint::new(0); 
-    let tmin = *t.iter().min().unwrap();
     for i in (x+1).saturating_sub(t[0])..=x {
-        ans += dp(i, n, &t, tmin);
+        ans += dp(i, n, &t, mint::new(n).inv());
     }
     println!("{}", ans);
 }
 
 #[memoise_map(x)]
-fn dp(x: usize, n: usize,t: &Vec<usize>, m: usize) -> mint {
-    if x == 0{
-        mint::new(n).inv()
+fn dp(x: usize, n: usize,t: &Vec<usize>, ninv: mint) -> mint {
+    if x == 0 {
+        ninv
     }
     else {
         let mut res = mint::new(0);
         for i in 0..n {
             if x >= t[i] {
-                res += dp(x-t[i], n, t, m);
+                res += dp(x-t[i], n, t, ninv);
             }
         }
-        res * mint::new(n).inv()
+        res * ninv
     }
 }
