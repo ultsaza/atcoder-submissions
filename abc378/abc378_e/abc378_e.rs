17b5f6ebca33e70d1bb94b1987cbf660a526eb78
Torimitsu <132206022+ultsaza@users.noreply.github.com>
2025-05-26 00:03:20 +0000

    [abc378] abc378_e

--- abc378/abc378_e/abc378_e.rs
+++ abc378/abc378_e/abc378_e.rs
@@ -1,5 +1,6 @@
-use ac_library::FenwickTree;
+use ac_library::{Additive, Segtree};
 use proconio::input;
+use std::iter::once;
 
 fn main() {
     input! {
@@ -7,16 +8,18 @@ fn main() {
         m: usize,
         a: [usize; n],
     }
-    let mut s = vec![0; n+1];
-    for i in 0..n {
-        s[i+1] = (s[i] + a[i]) % m;
-    }
-    let mut fw = FenwickTree::new(m,0);
+
+    let s = once(0).chain(a.iter().scan(0, |acc, &x| {
+        *acc = (*acc + x) % m;
+        Some(*acc)
+    })).collect::<Vec<_>>();
+
+    let mut seg = Segtree::<Additive<i64>>::new(m);
     let mut ans: i64 = 0;
     let mut sum_s: i64 = 0;
     for r in 1..=n {
-        ans += r as i64 * s[r] as i64 - sum_s + m as i64 * fw.sum(s[r]+1..m) as i64;
-        fw.add(s[r], 1);
+        ans += r as i64 * s[r] as i64 - sum_s + m as i64 * seg.prod(s[r]+1..m) as i64;
+        seg.set(s[r], seg.get(s[r]) + 1);
         sum_s += s[r] as i64;
     }
     println!("{}", ans);
