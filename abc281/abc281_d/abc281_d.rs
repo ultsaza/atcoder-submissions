d23a40e1be85d69a2925c71d191f7a0a85d231de
Torimitsu <132206022+ultsaza@users.noreply.github.com>
2025-05-27 23:49:43 +0000

    [abc281] abc281_d

--- abc281/abc281_d/abc281_d.rs
+++ abc281/abc281_d/abc281_d.rs
@@ -1,3 +1,4 @@
+use itertools::iproduct;
 use proconio::input;
 
 fn main() {
@@ -9,13 +10,9 @@ fn main() {
     }
     let mut dp: Vec<Vec<Vec<i64>>> = vec![vec![vec![i64::MIN;k+1];d];n+1];
     dp[0][0][0] = 0;
-    for i in 0..n {
-        for j in 0..d {
-            for l in 0..k {
-                dp[i+1][(j + a[i])%d][l+1] = dp[i][(j +a[i])%d][l+1].max(dp[i][j][l] + a[i] as i64);
-                dp[i+1][j][l] = dp[i][j][l].max(dp[i+1][j][l]);
-            }
-        }
+    for (i, j, l) in iproduct!(0..n, 0..d, 0..k) {
+        dp[i+1][(j + a[i])%d][l+1] = dp[i][(j +a[i])%d][l+1].max(dp[i][j][l] + a[i] as i64);
+        dp[i+1][j][l] = dp[i][j][l].max(dp[i+1][j][l]);
     }
     println!("{}", dp[n][0][k].max(-1));
 }
