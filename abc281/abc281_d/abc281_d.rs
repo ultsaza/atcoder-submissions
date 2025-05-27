bc7fc85cd0aa056fc8ae32e6a1744a0676d5d159
Torimitsu <132206022+ultsaza@users.noreply.github.com>
2025-05-27 23:33:01 +0000

    [abc281] abc281_d

---
+++ abc281/abc281_d/abc281_d.rs
@@ -0,0 +1,21 @@
+use proconio::input;
+
+fn main() {
+    input! {
+        n: usize,
+        k: usize,
+        d: usize,
+        mut a: [usize; n],
+    }
+    let mut dp: Vec<Vec<Vec<i64>>> = vec![vec![vec![i64::MIN;k+1];d];n+1];
+    dp[0][0][0] = 0;
+    for i in 0..n {
+        for j in 0..d {
+            for l in 0..k {
+                dp[i+1][(j + a[i])%d][l+1] = dp[i][(j +a[i])%d][l+1].max(dp[i][j][l] + a[i] as i64);
+                dp[i+1][j][l] = dp[i][j][l].max(dp[i+1][j][l]);
+            }
+        }
+    }
+    println!("{}", dp[n][0][k].max(-1));
+}
