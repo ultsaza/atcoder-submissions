75fbb4ca3f1afbbb977cc6157fa51fc49fc6db86
Torimitsu <132206022+ultsaza@users.noreply.github.com>
2025-06-02 07:29:07 +0000

    [abc408] abc408_c

---
+++ abc408/abc408_c/abc408_c.rs
@@ -0,0 +1,18 @@
+use proconio::{input, marker::Usize1};
+
+fn main() {
+    input! {
+        n: usize,
+        m: usize,
+        lr: [(Usize1, Usize1); m],
+    }
+    let mut imos = vec![0; n + 1];
+    for (l, r) in lr {
+        imos[l] += 1;
+        imos[r + 1] -= 1;
+    }
+    for i in 0..n {
+        imos[i + 1] += imos[i];
+    }
+    println!("{}", imos[..n].iter().min().unwrap());
+}
