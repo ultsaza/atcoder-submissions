a2ecc48142a29c8bd5964cb9cb178cf4d17894fe
Torimitsu <132206022+ultsaza@users.noreply.github.com>
2025-09-26 10:41:23 +0000

    [typical90] typical90_h

--- typical90/typical90_h/typical90_h.cpp
+++ typical90/typical90_h/typical90_h.cpp
@@ -1,35 +1,35 @@
 #include <bits/stdc++.h>
+#include <atcoder/all>
 using namespace std;
-using ll = long long;
-#define rep(i, r) for (ll i = 0; i < (r); ++i)
+using namespace atcoder;
+#define rep(i, l, r) for (ll i = (l); i < (r); ++i)
 #define all(x) (x).begin(), (x).end()
-const ll M = 1000000007;
+using ll = long long;
+using pl = pair<ll,ll>;
+using vi = vector<int>;
+using vvi = vector<vector<int>>;
+using vl = vector<ll>;
+using vvl = vector<vector<ll>>;
+#define INF 4*10e18
+#pragma GCC optimize("O3")
+#pragma GCC optimize("unroll-loops")
+using mint=modint1000000007;
 
 int main() {
-   int N;
-   string S;
-   string T = "atcoder";
-   cin >> N >> S;
-   unsigned int dp[N + 1][T.size() + 1];
-
-   
-   rep(i, N + 1) {
-    rep(j, T.size() + 1) {
-        dp[i][j] = 0;
-    }
-   }
-   dp[0][0] = 1;
-
-   rep(i, N) {
-    rep(j, T.size() + 1) {
-        dp[i + 1][j] = (dp[i + 1][j]% M + dp[i][j]% M) % M;
-
-        if(S[i] == T[j] && j < T.size()) {
-            dp[i + 1][j + 1] = (dp[i + 1][j + 1]% M + dp[i][j]% M) % M;
-
+     ll n; 
+     cin >> n;
+     string s;
+     cin >> s;
+     vector<vector<mint>> dp(n+1, vector<mint> (8,0));
+     string t = "atcoder";
+     dp[0][0]++;
+     rep(i,0,n) {
+        rep(j,0,t.size()+1) {
+            if (s[i]==t[j]&&j!=t.size()) {
+                dp[i+1][j+1] += dp[i][j];
+            }
+            dp[i+1][j] += dp[i][j];
         }
-    }
-   }
-
-   cout << dp[N][T.size()];
-}
+     }
+     cout << dp[n][7].val() << endl;
+} 
