c649f6a4340674d22cbb4ed039fc34afcac366a1
Torimitsu <132206022+ultsaza@users.noreply.github.com>
2026-01-21 22:50:11 +0000

    [abc213] abc213_c

--- abc213/abc213_c/abc213_c.rs
+++ abc213/abc213_c/abc213_c.rs
@@ -3,27 +3,22 @@ use superslice::Ext;
 
 fn main() {
     input! {
-        h: usize,
-        w: usize,
+        _h: usize,
+        _w: usize,
         n: usize,
         ab: [(usize, usize); n]
     }
 
-    let mut a = vec![];
-    let mut b =vec![];
-    for (x,y) in &ab {
-        a.push(x);
-        b.push(y);
-    }
+    let (mut a, mut b): (Vec<_>, Vec<_>) = ab.iter().copied().unzip();
     a.sort();
     a.dedup();
     b.sort();
     b.dedup();
-    for (x, y) in &ab {
-        println!(
-            "{} {}",
-            a.lower_bound(&x) + 1,
-            b.lower_bound(&y) + 1
-        )
+
+    let ca = |x| a.lower_bound(&x) + 1;
+    let cb = |y| b.lower_bound(&y) + 1;
+
+    for (x, y) in ab {
+        println!("{} {}", ca(x), cb(y));
     }
 }
