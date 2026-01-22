d75e6aec6cb9b2dc037cb73214a2c808d09fd298
Torimitsu <132206022+ultsaza@users.noreply.github.com>
2026-01-22 17:39:12 +0000

    [abc440] abc440_b

--- abc440/abc440_b/abc440_b.rs
+++ abc440/abc440_b/abc440_b.rs
@@ -4,13 +4,14 @@ use itertools::Itertools;
 fn main() {
     input! {
         n: usize,
-        a: [usize; n]
+        t: [usize; n]
     }
-    let t: Vec<_> = a
-        .iter()
-        .enumerate()
-        .map(|(i, &x)| (i + 1, x))
+    println!(
+        "{}",
+        t.iter().enumerate()
         .sorted_by_key(|x| x.1)
-        .collect();
-    println!("{} {} {}", t[0].0, t[1].0, t[2].0);
+        .take(3)
+        .map(|x| x.0 + 1)
+        .join(" ")
+    )
 }
